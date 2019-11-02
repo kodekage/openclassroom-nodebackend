8819b7472ff8299a741d411eb4cf9a798f5c38ff
Opara Prosper <oparaprosper79@gmail.com>
2019-11-02 14:39:59 +0100

    Handle user file upload and delete image assets properly

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -1,12 +1,16 @@
 const Thing = require('../models/things');
+const fs = require('fs');
 
 exports.createThing = (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
+  req.body.thing = JSON.parse(req.body.thing);
+
   const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    imageUrl: req.body.imageUrl,
-    userId: req.body.userId
+    title: req.body.thing.title,
+    description: req.body.thing.description,
+    price: req.body.thing.price,
+    imageUrl: url + '/images/' + req.file.filename,
+    userId: req.body.thing.userId
   });
 
   thing.save().then(() => {
@@ -32,14 +36,30 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyOneThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    imageUrl: req.body.imageUrl,
-    userId: req.body.userId
-  })
+  let thing = new Thing({_id: req.params._id});
+
+  if(req.file){
+    req.body.thing = JSON.parse(req.body.thing);
+    const url = req.protocol + '://' + req.get('host');
+    
+    thing = {
+      _id: req.params.id,
+      title: req.body.thing.title,
+      description: req.body.thing.description,
+      price: req.body.thing.price,
+      imageUrl: url + '/images/' + req.file.filename,
+      userId: req.body.thing.userId
+    };
+  }else{
+    thing = {
+      _id: req.params.id,
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      imageUrl: req.body.imageUrl,
+      userId: req.body.userId
+    }
+  }
 
   Thing.updateOne({
     _id: req.params.id
@@ -55,17 +75,22 @@ exports.modifyOneThing = (req, res, next) => {
 };
 
 exports.deleteOneThing = (req, res, next) => {
-  Thing.deleteOne({
-    _id: req.params.id
-  }).then(() => {
-    res.status(201).json({
-      message: 'deleted'
-    })
-  }).catch(error => {
-    res.status(400).json({
-      error: error
-    })
-  })
+  Thing.findOne({_id: req.params.id}).then((thing) => {
+    const filename = thing.imageUrl.split('/images/')[1];
+    fs.unlink('images/' + filename, () => {
+      Thing.deleteOne({
+        _id: req.params.id
+      }).then(() => {
+        res.status(201).json({
+          message: 'deleted'
+        });
+      }).catch(error => {
+        res.status(400).json({
+          error: error
+        });
+      });
+    });
+  });
 };
 
 exports.getAllThings = (req, res, next) => {
