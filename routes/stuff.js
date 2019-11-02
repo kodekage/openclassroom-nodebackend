2083d728d0b0357436b04240d461dd89ce4e9263
Opara Prosper <oparaprosper79@gmail.com>
2019-11-02 14:39:07 +0100

    Add multer and authentication middleware

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
 const StuffController = require('../controllers/stuff');
 
-router.post('/', StuffController.createThing);
-router.get('/:id', StuffController.getOneThing);
-router.put('/:id', StuffController.modifyOneThing);
-router.delete('/:id', StuffController.deleteOneThing);
-router.use('/', StuffController.getAllThings);
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, StuffController.createThing);
+router.get('/:id', auth, StuffController.getOneThing);
+router.put('/:id', auth, multer, StuffController.modifyOneThing);
+router.delete('/:id', auth, StuffController.deleteOneThing);
+router.get('/', auth, StuffController.getAllThings);
 
 module.exports = router;
